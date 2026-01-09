fcec17d491dc266caf0d50c27dabb6249cd80569
Kodossou <72127808+pmkod@users.noreply.github.com>
2026-01-09 15:07:17 +0000

    Practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { PracticeComponent } from "./practice/practice.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "app-root",
+	imports: [RouterOutlet, PracticeComponent],
+	// templateUrl: './app.component.html',
+	template: `
+  <!-- <h1>
+    My title is {{title}}
+    Age {{age}}
+  </h1>
+  <p>
+    Val {{counter}}
+
+    <button (click)="increment()">
+Increment
+    </button>
+
+        <button (click)="decrement()">
+Decrement
+    </button>
+  </p>
+
+  <div>
+
+  <input type="text" #myInput (input)="onEdit(myInput.value)">
+  <p>{{theValue}}</p>
+
+  <div>
+    <div [class]="messageClass">Message ...</div>
+    <button (click)="changeColor('success')">Success</button>
+    <button (click)="changeColor('error')">Error</button>
+  </div>
+</div> -->
+<app-practice />
+  `,
+	// styleUrl: "./app.component.scss",
+	styles: [".success{color: green} .error{color: red}"],
 })
 export class AppComponent {
-  title = 'employee_app';
+	title = "ABA";
+
+	age = 22;
+	counter = 0;
+
+	theValue = "";
+	messageClass: "success" | "error" = "success";
+
+	increment() {
+		this.counter += 1;
+	}
+	decrement() {
+		this.counter -= 1;
+	}
+
+	onEdit(value: string) {
+		console.log(value);
+		this.theValue = value;
+	}
+
+	changeColor(style: "success" | "error") {
+		this.messageClass = style;
+	}
 }
---
+++ src/app/practice/practice.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "app-practice",
+	imports: [],
+	templateUrl: "./practice.component.html",
+	styleUrl: "./practice.component.scss",
+})
+export class PracticeComponent {}
